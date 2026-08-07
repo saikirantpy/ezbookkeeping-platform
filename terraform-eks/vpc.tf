d52f57333ae28a171c0abe49c70c464a742c8575
Saikiran Thummanapally <saikiranthummanapally@gmail.com>
2026-08-07 12:44:56 +0530

    Create VPC resource

---
+++ terraform-eks/vpc.tf
@@ -0,0 +1,23 @@
+###############################################
+# VPC
+###############################################
+
+resource "aws_vpc" "main" {
+
+  cidr_block = var.vpc_cidr
+
+  enable_dns_support = true
+
+  enable_dns_hostnames = true
+
+  tags = merge(
+
+    local.common_tags,
+
+    {
+      Name = "${local.project_name}-vpc"
+    }
+
+  )
+
+}
