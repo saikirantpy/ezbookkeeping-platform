20f6698f7cdd26f1d3b030f17ba6109142d6947a
Saikiran Thummanapally <saikiranthummanapally@gmail.com>
2026-08-07 15:40:14 +0530

    Create public subnets for EKS

---
+++ terraform-eks/subnets.tf
@@ -0,0 +1,54 @@
+###############################################
+# Public Subnet 1
+###############################################
+
+resource "aws_subnet" "public_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_cidrs[0]
+
+  availability_zone = var.availability_zones[0]
+
+  map_public_ip_on_launch = true
+
+  tags = merge(
+
+    local.common_tags,
+
+    {
+      Name = "${local.project_name}-public-1"
+
+      "kubernetes.io/role/elb" = "1"
+    }
+
+  )
+
+}
+###############################################
+# Public Subnet 2
+###############################################
+
+resource "aws_subnet" "public_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_cidrs[1]
+
+  availability_zone = var.availability_zones[1]
+
+  map_public_ip_on_launch = true
+
+  tags = merge(
+
+    local.common_tags,
+
+    {
+      Name = "${local.project_name}-public-2"
+
+      "kubernetes.io/role/elb" = "1"
+    }
+
+  )
+
+}
